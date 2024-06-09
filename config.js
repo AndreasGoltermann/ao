d53691434f3b54d60bfb947144e47f3ea3a6d57f
Andreas Goltermann <Andreas.goltermann@gmail.com>
2024-06-10 01:27:56 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const APP_ROOT = 'https://aohcommunity.com';
-const CLIENT_ID = 'test-demo-spa';
+const CLIENT_ID = '74a93dc9-eb83-43b3-8aac-49ccbd1e3e50';
 
 // Calculate frequently-used URLs from the above:
 const APP_HOME = `${APP_ROOT}/index.html`;
