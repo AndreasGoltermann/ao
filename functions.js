c42e0d5bacfd6127ea6511fbbfdfd54050500be1
Andreas Goltermann <Andreas.goltermann@gmail.com>
2024-06-11 20:02:02 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadHTML('header.html', 'header');
     loadHTML('footer.html', 'footer');
     loadHTML('raid_teams.html', 'raid_teams');
+    loadHTML('menu.html', 'menu')
 });
 
 function toRaid() {
@@ -17,4 +18,16 @@ function toRaid() {
 
 function toGuide() {
     window.location.href = 'https://docs.google.com/spreadsheets/d/1L6DyBnbm97NNwsBlPwCpFf_W9XxKSMnqZJ8G4Ul7t6Q/edit?usp=sharing';
+}
+
+function toMSF() {
+    window.location.href = 'https://www.msf.gg';
+}
+
+function toHome() {
+    window.location.href = 'index.html';
+}
+
+function toDisc() {
+    window.location.href = 'https://discordapp.com/channels/754682954378248203/1246197723087241508';
 }
