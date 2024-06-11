6fcda2c647d31d9b2c8935c835fd7d32f13df54f
Andreas Goltermann <Andreas.goltermann@gmail.com>
2024-06-11 13:15:04 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const APP_ROOT = 'https://aohcommunity.com';
-const CLIENT_ID = '14419d51-fe48-4b42-b17f-a1714bc4f228';
-
-// Calculate frequently-used URLs from the above:
-const APP_HOME = `${APP_ROOT}/index.html`;
-const APP_CALLBACK = `${APP_ROOT}/redirect.html`;
